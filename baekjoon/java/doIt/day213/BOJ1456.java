49286cb2f2ad5aab3cf87f5729507129b1cbc83d
Hyunjae Kim <khj99present@gmail.com>
2024-12-27 23:34:32 +0900

    feat. day213
    소수 판별 관련 2 문제

---
+++ baekjoon/java/doIt/day213/BOJ1456.java
@@ -0,0 +1,61 @@
+package doit.day213;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1456 {
+
+    private static final int MAX_NUM = 10_000_000;
+
+    private static long A, B;
+    private static boolean[] isPrime;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+
+        sieveOfEratosthenes(MAX_NUM);
+
+        int cnt = 0;
+
+        for (int i = 2; i <= MAX_NUM; i++) {
+            if (isPrime[i]) {
+                long temp = i;
+
+                while ((double) i <= (double) B / temp) {
+                    if ((double) i >= (double) A / temp) {
+                        cnt++;
+                    }
+                    temp *= i;
+                }
+            }
+        }
+
+        bw.write(cnt + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    private static void sieveOfEratosthenes(int N) {
+        isPrime = new boolean[N + 1];
+
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+}
---
+++ baekjoon/java/doIt/day213/BOJ1929.java
@@ -0,0 +1,48 @@
+package doit.day213;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1929 {
+
+    private static int M, N;
+    private static boolean[] isPrime;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        sieveOfEratosthenes(N);
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                bw.write(i + "\n");
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    private static void sieveOfEratosthenes(int num) {
+        isPrime = new boolean[num + 1];
+
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= num; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+}
