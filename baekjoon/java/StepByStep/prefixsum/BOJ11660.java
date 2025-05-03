0f087984b5f1fbfee11451247052298c3c4d6dec
Hyunjae Kim <khj99present@gmail.com>
2025-05-03 21:44:51 +0900

    feat. Day299
    단계별로 풀어보기-누적합 3문제 풀이

---
+++ baekjoon/java/StepByStep/prefixsum/BOJ11660.java
@@ -0,0 +1,48 @@
+package StepByStep.prefixsum;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ11660 {
+
+    private static int N, M;
+    private static int[][] grid, prefixSum;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        grid = new int[N + 1][N + 1];
+        prefixSum = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= N; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+                prefixSum[i][j] = grid[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+            }
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+
+            bw.write(result + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
