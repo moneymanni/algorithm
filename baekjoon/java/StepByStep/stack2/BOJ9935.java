ad8d5d20edca87ea3d264ccb3f5a715446e7fd0b
Hyunjae Kim <khj99present@gmail.com>
2025-05-16 23:42:18 +0900

    feat. Day310
    단계별로 풀어보기-스택2 1문제 풀이

---
+++ baekjoon/java/StepByStep/stack2/BOJ9935.java
@@ -0,0 +1,55 @@
+package StepByStep.stack2;
+
+import java.io.*;
+import java.util.Stack;
+
+public class BOJ9935 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        String bomb = br.readLine();
+
+        String result = explode(str, bomb);
+
+        bw.write(result);
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    private static String explode(String str, String bomb) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            stack.push(ch);
+
+            if (stack.size() >= bomb.length() && check(stack, bomb)) {
+                for (int j = 0; j < bomb.length(); j++) {
+                    stack.pop();
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : stack)
+            sb.append(ch);
+
+        return sb.length() == 0 ? "FRULA" : sb.toString();
+    }
+
+    private static boolean check(Stack<Character> stack, String bomb) {
+        for (int i = 0; i < bomb.length(); i++) {
+            if (stack.get(stack.size() - bomb.length() + i) != bomb.charAt(i))
+                return false;
+        }
+
+        return true;
+    }
+}
