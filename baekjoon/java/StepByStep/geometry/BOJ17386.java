56995ac3d1d9e194758843c9b03c4d749edbe182
Hyunjae Kim <khj99present@gmail.com>
2025-08-10 04:41:04 +0900

    fix. Day376
    백준 17386번 문제 수정

--- baekjoon/java/StepByStep/geometry/BOJ17386.java
+++ baekjoon/java/StepByStep/geometry/BOJ17386.java
@@ -13,18 +13,18 @@ public class BOJ17386 {
         st = new StringTokenizer(br.readLine());
         Point L1Point1 = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         Point L1Point2 = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        Line L1 = new Line(L1Point1, L1Point2);
 
         st = new StringTokenizer(br.readLine());
         Point L2Point1 = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         Point L2Point2 = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        Line L2 = new Line(L2Point1, L2Point2);
 
-        if (ccw(L1.endPoint1, L1.endPoint2, L2.endPoint1) * ccw(L1.endPoint1, L1.endPoint2, L2.endPoint2) < 0
-                && ccw(L2.endPoint1, L2.endPoint2, L1.endPoint1) * ccw(L2.endPoint1, L2.endPoint2, L1.endPoint2) < 0) {
-            bw.write("1\n");
+        int first = ccw(L1Point1, L1Point2, L2Point1) * ccw(L1Point1, L1Point2, L2Point2);
+        int second = ccw(L2Point1, L2Point2, L1Point1) * ccw(L2Point1, L2Point2, L1Point2);
+
+        if (first <= 0 && second <= 0) {
+            bw.write(1 + "\n");
         } else {
-            bw.write("0\n");
+            bw.write(0 + "\n");
         }
 
         br.close();
@@ -32,28 +32,26 @@ public class BOJ17386 {
         bw.close();
     }
 
-    private static int ccw(Point point1, Point point2, Point point3) {
-        return (point1.x * point2.y + point2.x * point3.y + point3.x * point1.y)
-                - (point1.y * point2.x + point2.y * point3.x + point3.y * point1.x) < 0 ? 1 : -1;
-    }
+    private static int ccw(Point p1, Point p2, Point p3) {
+        long result = (p1.x * p2.y) + (p2.x * p3.y) + (p3.x * p1.y)
+                - (p1.y * p2.x) - (p2.y * p3.x) - (p3.y * p1.x);
 
-    private static class Line {
-
-        Point endPoint1;
-        Point endPoint2;
-
-        public Line(Point endPoint1, Point endPoint2) {
-            this.endPoint1 = endPoint1;
-            this.endPoint2 = endPoint2;
+        if(result > 0) {
+            return 1;
+        }
+        else if(result < 0) {
+            return -1;
+        } else {
+            return 0;
         }
     }
 
     private static class Point {
 
-        int x;
-        int y;
+        long x;
+        long y;
 
-        public Point(int x, int y) {
+        public Point(long x, long y) {
             this.x = x;
             this.y = y;
         }
